ec7072580fbf4cf3b210127ce8391967a457d71b
Michael Andrew Chan <67429929+michac789@users.noreply.github.com>
2022-06-23 15:23:10 +0800

    tidy up sso pages and views

--- sso/models.py
+++ sso/models.py
@@ -1,7 +1,13 @@
-from re import L
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class User(AbstractUser):
-    pass
+    TIER = [
+        ("T5", "SuspendedUser"),
+        ("T4", "User"),
+        ("T3", "StarUser"),
+        ("T2", "Admin"),
+        ("T1", "SuperAdmin"),
+    ]
+    status = models.CharField(max_length=2, choices=TIER, default="T4")
