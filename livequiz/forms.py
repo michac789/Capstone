0f7e0ee81f9e5ee2c4b7137b327169dd34ed21c2
Michael Andrew Chan <67429929+michac789@users.noreply.github.com>
2022-06-24 22:22:34 +0800

    add stylings: enter code page

--- livequiz/forms.py
+++ livequiz/forms.py
@@ -1,8 +1,6 @@
 from django import forms
 from django.utils.safestring import mark_safe
 
-from .models import QuestionType1
-
 
 class GameForm(forms.Form):
     title = forms.CharField(label = mark_safe("Title"), max_length = 64,
@@ -13,10 +11,12 @@ class GameForm(forms.Form):
 
 
 class CodeForm(forms.Form):
-    sessioncode = forms.CharField(label = "Enter code", max_length = 6)
+    sessioncode = forms.CharField(label = "Enter code", max_length = 6,
+                                  widget = forms.TextInput(attrs = {
+                                      "placeholder": "6-character code"
+                                  }))
 
 
 class AddQuesForm(forms.Form):
     ques_count = forms.IntegerField(label = "Add these amount of questions",
                                     min_value = 1, max_value = 10)
-
